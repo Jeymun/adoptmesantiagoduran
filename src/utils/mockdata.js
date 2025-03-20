b42e0e8aa343c55c7240e74df8e2f1005c83f0e7
Santiago Duran <santiagojeymun@gmail.com>
2025-03-20 12:28:47 -0300

    Roles

--- src/utils/mockdata.js
+++ src/utils/mockdata.js
@@ -1,6 +1,28 @@
 import { faker } from '@faker-js/faker';
 import bcrypt from 'bcryptjs';
 
+// Función para generar usuarios falsos
+export const generateMockUsers = (num) => {
+  return Array.from({ length: num }).map(() => {
+    const password = "coder123";  // Contraseña a encriptar
+    const hashedPassword = bcrypt.hashSync(password, 10);  // Encriptar la contraseña
+
+    const role = Math.random() > 0.5 ? 'admin' : 'user';  // Asignar un rol aleatorio
+
+    return {
+      id: faker.string.uuid(),
+      username: faker.internet.userName(),
+      email: faker.internet.email(),
+      password: hashedPassword,
+      firstName: faker.name.firstName(),
+      lastName: faker.name.lastName(),
+      age: faker.number.int({ min: 18, max: 100 }),
+      role: role,  // Agregar el campo role
+    };
+  });
+};
+
+// Función para generar mascotas ficticias
 export const generateMockPets = () => {
   return Array.from({ length: 10 }).map(() => ({
     id: faker.string.uuid(),
@@ -8,23 +30,4 @@ export const generateMockPets = () => {
     type: faker.animal.type(),
     age: faker.number.int({ min: 1, max: 15 }),
   }));
-};
-
-export const generateMockUsers = (numUsers) => {
-  const users = Array.from({ length: numUsers }).map(() => {
-    const password = 'coder123';
-    const hashedPassword = bcrypt.hashSync(password, 10);
-
-    return {
-      id: faker.string.uuid(),
-      username: faker.internet.userName(),
-      email: faker.internet.email(),
-      password: hashedPassword,  // Contraseña encriptada
-      firstName: faker.name.firstName(),
-      lastName: faker.name.lastName(),
-      age: faker.number.int({ min: 18, max: 99 }),
-    };
-  });
-
-  return users;
 };
