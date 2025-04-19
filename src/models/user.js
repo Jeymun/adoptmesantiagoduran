914efbfebdf67d6cbbab1c0fb6ae2146f0ff3cf0
Jeymun <santiagojeymun@gmail.com>
2025-04-19 02:55:29 -0300

    cambios

--- src/models/user.js
+++ src/models/user.js
@@ -1,18 +1,11 @@
-import mongoose from 'mongoose';
+import mongoose from "mongoose";
+
+const userCollection = "users";
 
 const userSchema = new mongoose.Schema({
-  username: String,
+  name: String,
   email: String,
   password: String,
-  firstName: String,
-  lastName: String,
-  age: Number,
-  role: { type: String, enum: ['user', 'admin'] },
-  pets: [mongoose.Schema.Types.ObjectId], // Referencia a los Pets
-  createdAt: { type: Date, default: Date.now },
-  updatedAt: { type: Date, default: Date.now },
 });
 
-const User = mongoose.model('User', userSchema);
-
-export default User;
+export const userModel = mongoose.model(userCollection, userSchema);
--- src/utils/mockdata.js
+++ src/utils/mockdata.js
@@ -3,30 +3,14 @@ import bcrypt from 'bcryptjs';
 
 export const generateMockUsers = (num) => {
   return Array.from({ length: num }).map(() => {
-    const password = "coder123";  
-    const hashedPassword = bcrypt.hashSync(password, 10);  
-
-    const role = Math.random() > 0.5 ? 'admin' : 'user';  
+    const password = 'coder123';
+    const hashedPassword = bcrypt.hashSync(password, 10);
+    const role = Math.random() > 0.5 ? 'admin' : 'user';
 
     return {
-      id: faker.string.uuid(),
-      username: faker.internet.username(),
       email: faker.internet.email(),
       password: hashedPassword,
-      firstName: faker.person.firstName(),
-      lastName: faker.person.lastName(),
-      age: faker.number.int({ min: 18, max: 100 }),
-      role: role, 
-      pets: []
+      role: role
     };
   });
-};
-
-export const generateMockPets = (num) => {
-  return Array.from({ length: num }).map(() => ({
-    id: faker.string.uuid(),
-    name: faker.person.firstName(),
-    type: faker.animal.type(),
-    age: faker.number.int({ min: 1, max: 15 }),
-  }));
 };
